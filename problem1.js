385d7c810b015905348ed233fc0d1a3061200655
Saquib <106192257+Saquib1996@users.noreply.github.com>
2022-06-26 20:47:33 +0530

    Add files via upload

---
+++ problem1.js
@@ -0,0 +1,11 @@
+let items=[
+              ["Shyam","Raju"],
+              ["Babu Rao","Devi Prasad"],
+              ["Kanji Bhai","Total Seth"]
+         ];
+console.log(items[0][0]);
+console.log(items[0][1]);
+console.log(items[1][0]);
+console.log(items[1][1]);
+console.log(items[2][0]);
+console.log(items[2][1]);
